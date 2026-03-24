42cca62e10f213fed7ab80f982b32b14f9dc9521
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-03-24 12:02:09 +0300

    Add getter and setter for the age property

--- module1/src/getter-setter.js
+++ module1/src/getter-setter.js
@@ -1,5 +1,19 @@
 let contact = {
+  _age: 36,
+  firstName: "David",
+  lastName: "Taylor",
   _tel: "207-662-5412",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  get age() {
+    return this._age;
+  },
+  set age(a) {
+    if (a > 0) {
+      this._age = a;
+    }
+  },
   get tel() {
     return this._tel;
   },
@@ -13,3 +27,6 @@ contact.tel = "100-100-1000";
 console.log(contact.tel);
 contact.email = "abelkevinkipkosgei@gmail.com";
 console.log(contact.email);
+console.log(contact.fullName);
+contact.age = -20;
+console.log(contact.age);
