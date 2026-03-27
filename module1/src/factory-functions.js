d9bf1f3c4866df48658234a123d0be2f3de7fbd7
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-03-27 11:35:14 +0300

    Add code to illustrate factory functions, closures

---
+++ module1/src/factory-functions.js
@@ -0,0 +1,18 @@
+let createColoredPoint = function (x, y, color) {
+  let _info = "...object under construction";
+  let _color = color;
+  console.log(info);
+  return {
+    x,
+    y,
+    getColor() {
+      return _color;
+    },
+  };
+};
+
+let coloredPoint1 = createColoredPoint(1, 1, "Red");
+let coloredPoint2 = createColoredPoint(2, 2, "Green");
+
+console.log(coloredPoint1.getColor());
+console.log(coloredPoint2.getColor());
