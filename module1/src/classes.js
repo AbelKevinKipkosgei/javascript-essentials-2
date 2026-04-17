e4931bf8504da7bb7f0565f90943765a6d455d24
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-04-17 04:42:46 +0300

    Implement classes in JavaScript

---
+++ module1/src/classes.js
@@ -0,0 +1,29 @@
+class Vehicle {
+  constructor({ id, latitude, longitude }) {
+    this.id = id;
+    this.status = "unavailable";
+    this.setPosition({ latitude, longitude });
+  }
+
+  setPosition({ latitude, longitude }) {
+    this.time = Date.now();
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  getPosition() {
+    return {
+      latitude: this.latitude,
+      longitude: this.longitude,
+    };
+  }
+}
+
+let vehicle1 = new Vehicle({
+  longitude: 18.213423,
+  latitude: 59.367628,
+  id: "AL1024",
+});
+
+vehicle1.setPosition({ longitude: 18.193121, latitude: 59.378654 });
+console.log(vehicle1.getPosition());
